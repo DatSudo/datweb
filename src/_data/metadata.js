0d22311a7db6715548232a26f63a99f65277e7c0
Datsudo <76833632+DatSudo@users.noreply.github.com>
2023-03-10 18:32:28 +0800

    Update metadata

--- src/_data/metadata.js
+++ src/_data/metadata.js
@@ -1,6 +1,7 @@
 module.exports = {
     "title": "Datsudo",
     "url": "https://datsudo.netlify.app/",
+    "repo_link": "https://github.com/datsudo/datweb",
     "language": "en",
     "description": "Datsudo's personal website.",
     "author": {
