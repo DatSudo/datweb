ec42f187e888a14ee8170e8f9eb68d97e5a1f490
Datsudo <76833632+DatSudo@users.noreply.github.com>
2023-03-10 18:21:38 +0800

    Add metadata.js

---
+++ src/_data/metadata.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "title": "Datsudo",
+    "url": "https://datsudo.netlify.app/",
+    "language": "en",
+    "description": "Datsudo's personal website.",
+    "author": {
+        "name": "Datsudo",
+        "url": "https://datsudo.netlify.app/about/"
+    }
+}
