34f9ab15a425a46a653be44b76d51ac34a3942a4
Datsudo <76833632+DatSudo@users.noreply.github.com>
2023-03-10 17:49:57 +0800

    Add accessibility titles for links

--- src/_includes/components/LatestPostCard.js
+++ src/_includes/components/LatestPostCard.js
@@ -3,10 +3,10 @@ const {html} = require("common-tags");
 function LatestPostCard(title, date, description, link) {
     return html`
     <div class="post-card__container">
-        <h3 class="post-card__title"><a href="${link}">${title}</a></h3>
+        <h3 class="post-card__title"><a title="Full post link" href="${link}">${title}</a></h3>
         <span class="post-date">${date}</span>
         <p class="post-card__description">${description}</p>
-        <span class="post-card__link"><a href="${link}">Read more</a></span>
+        <span class="post-card__link"><a title="Full post link" href="${link}">Read more</a></span>
     </div>
     `;
 }
